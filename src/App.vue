b9f4956081e5d80dfceb406f5424499b5619d785
7t33n <mroxotnik.app@gmail.com>
2022-10-11 12:40:47 +0400

    feat PX-1 | project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="layout">
+    <router-view />
+  </component>
+</template>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { shallowRef } from "vue";
+
+const route = useRoute();
+const layout = shallowRef('');
+
+import(`@/components/views/layout/${route.meta.layout || 'default'}.vue`)
+  .then(val => layout.value = val.default);
+</script>
+<style scoped lang="scss">
+</style>
